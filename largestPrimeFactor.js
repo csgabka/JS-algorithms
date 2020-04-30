fbbece7e56a1ca71228d829f226bce3f833f6f1c
Gabi <csgabka@gmail.com>
2020-04-30 15:14:51 +0100

    2

---
+++ largestPrimeFactor.js
@@ -0,0 +1,11 @@
+function largestPrimeFactor(number) {
+  let prime = 2,
+    max = 1;
+  while (prime <= number) {
+    if (number % prime == 0) {
+      max = prime;
+      number = number / prime;
+    } else prime++; 
+  }
+  return max;
+}
